13be473387b244735db7530e67aeb8e2be8abf2f
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-13 14:56:08 +0200

    refactor util gePackageJson

--- src/utils/tool-utils.ts
+++ src/utils/tool-utils.ts
@@ -1,10 +1,7 @@
-import * as fs from 'fs';
-
 export class ToolUtils {
     constructor() {}
 
     static getPackageJson(path: string): any {
-        const file = fs.readFileSync(path, 'utf-8');
-        return JSON.parse(file);
+        return require(path);
     }
 }
