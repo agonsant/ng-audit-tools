ef59cdb9772d4cc243e711d4081a8b8f6aa39495
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-23 10:08:37 +0200

    refactor lazy loading test

--- src/folder-structure/test-cases/app-lazyLoading-test.ts
+++ src/folder-structure/test-cases/app-lazyLoading-test.ts
@@ -18,18 +18,19 @@ export class AppLazyLoadingTest implements ITestCase {
         const sourceAppPath = path.join(context.getWorkspace(), 'src/app');
         const appRoutingModulePath = path.join(sourceAppPath, 'app-routing.module.ts');
         return new Promise((resolve, reject) => {
-            if (!ToolUtils.hasLazyLoading(appRoutingModulePath)) reject(new LazyLoadingException(this.description, appRoutingModulePath));
+            if (!ToolUtils.hasLazyLoading(appRoutingModulePath)) {
+                return reject(new LazyLoadingException(this.description, appRoutingModulePath));
+            }
             
             glob(`${sourceAppPath}/**/*-routing.module.ts`, (err: Error, filesPath: string[]) => {
-                if (err) reject(new LazyLoadingException());
+                if (err) return reject(new LazyLoadingException());
                 let index = 0;
                 let validate = filesPath.length > 0;
                 while (validate && index < filesPath.length) {
                     if(validate = ToolUtils.hasLazyLoading(filesPath[index])) index += 1;
                 }
     
-                if (!validate) reject(new LazyLoadingException(this.description, filesPath[index]));
-                resolve();
+                return validate ? resolve() : reject(new LazyLoadingException(this.description, filesPath[index]));
             });
             
         });
