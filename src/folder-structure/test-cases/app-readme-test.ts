ce3aebe0aed6f2af3182fa2b7dea0968dc0ec3c2
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-24 09:11:45 +0200

    refactor readme test

--- src/folder-structure/test-cases/app-readme-test.ts
+++ src/folder-structure/test-cases/app-readme-test.ts
@@ -12,21 +12,24 @@ export class AppReadmeTest implements ITestCase {
     description: string;
   
     constructor() {
-        this.description = 'Should to exist Readme.md file by module';
+        this.description = 'Should to exist Readme.md file by feature module and not be empty';
     }
 
     run(context: IContext): Promise<string> {
-        const sourceFolder = path.join(context.getWorkspace(), '/src/app');
+        const sourceAppPath = path.join(context.getWorkspace(), '/src/app');
         return new Promise((resolve, reject) => {
-            glob(`${sourceFolder}/modules/**`, (err: any, files: any) => {
+            glob(`${sourceAppPath}/modules/**`, (err: Error, filesPath: string[]) => {
 
-                const dirs = files.filter((file: fs.PathLike) => ToolUtils.isModuleDirectoryPath(file.toString()));
                 if(err) reject(new ReadmeException());
+                const dirs = filesPath.filter((filePath: string) => ToolUtils.isModuleDirectoryPath(filePath));
                 let index = 0;
-                let validate = dirs.length > 0;
+                let validate = true;
                 while (validate && index < dirs.length) {
-                    const filesDir = fs.readdirSync(dirs[index], 'utf-8');
-                    if(validate = (filesDir.find(file => file === 'Readme.md') || '').length > 0) index += 1;
+                    const readmePath = path.join(dirs[index], 'Readme.md');
+                    const readmeFileExist = fs.existsSync(readmePath);
+                    const readmeFileIsEmpty = fs.readFileSync(readmePath, 'utf-8').length === 0;
+                    
+                    if(validate = (readmeFileExist && !readmeFileIsEmpty)) index += 1;
                 };
 
                 validate ? resolve() : reject(new ReadmeException(this.description, dirs[index]));
