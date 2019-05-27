1675635e33da7d1788ee600e1e516aaec9662cdc
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-27 12:18:36 +0200

    refactor cypres test

--- src/folder-structure/exceptions/cypress-exception.ts
+++ src/folder-structure/exceptions/cypress-exception.ts
@@ -2,8 +2,8 @@
 
 export class CypressException extends Error {
 
-    constructor(message: string = 'Generic Error', cypressVersion: string = 'Not found') {
-        super(`CypressException: ${message}. The cypress version founded is: ${cypressVersion}`);
+    constructor(message: string = 'Generic Error', cypressVersion: string = 'Not found', resultCommand: string = 'Command not executed') {
+        super(`CypressException: ${message}. The cypress version founded is: ${cypressVersion}. Command result: ${resultCommand}`);
     }
 
 }
--- src/folder-structure/test-cases/app-cypress-test.ts
+++ src/folder-structure/test-cases/app-cypress-test.ts
@@ -2,14 +2,17 @@ import { ITestCase } from '../../types/i-test-case';
 import { ToolUtils } from '../../utils/tool-utils';
 import { IContext } from '../../types/i-context';
 import { CypressException } from '../exceptions/cypress-exception';
+
 import * as path from 'path';
+import * as child_process from 'child_process';
+const find_process = require('find-process');
 
 export class AppCypressTest implements ITestCase {
 
     description: string;
     cypressKey: string;
     constructor() {
-        this.description = 'Cypress should be to exist in te project';
+        this.description = 'Cypress should be to exist in te project and pass tests';
         this.cypressKey = 'cypress';
     }
 
@@ -18,11 +21,43 @@ export class AppCypressTest implements ITestCase {
         const sourceFolder = path.join(workSpace, 'package.json');
         return new Promise((resolve, reject) => {
             const packageJson = ToolUtils.getPackageJson(sourceFolder);
-            const cypress = packageJson.devDependencies[this.cypressKey]; 
+            const cypress = packageJson.dependencies[this.cypressKey]; 
             if (cypress) {
-               resolve();
+                const start = child_process.exec('npm run start', {cwd: workSpace});
+                start.on('exit', () => {
+                    find_process('port', 4200)
+                        .then((foundProcess: any) => {
+                            foundProcess.forEach((proc: any) => process.kill(proc.pid));
+                            process.exit(0);
+                        });
+                });
+                start.on('error', err => {
+                    start.kill();
+                    return reject(new CypressException(this.description, cypress, err.message));
+                });
+
+                start.stderr!.on('data', data => {
+                    start.kill();
+                    return reject(new CypressException(this.description, cypress, data));
+                });
+                start.stdout!.on('data', data => {
+                    console.log(data);
+                    if(data.includes('Compiled successfully.')) {
+                        try {
+                            const cypressRun = child_process.execSync('npm run cypress', {cwd: workSpace, stdio: 'pipe'});
+                            start.kill();
+                            console.log(cypressRun.toString());
+                            return resolve();
+                        } catch(errCypress) {
+                            const commandCypressError = errCypress.stdout ? errCypress.stdout : errCypress.stderr;
+                            start.kill();
+                            return reject(new CypressException(this.description, cypress, commandCypressError));
+                        }
+                    }
+                });
+                
             } else {
-                reject(new CypressException(this.description, cypress));
+                return reject(new CypressException(this.description));
             }
         });
     }
