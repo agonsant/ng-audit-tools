bbfbcedbdff85932534308851416c0be9dffd5fb
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-27 17:51:20 +0200

    change message exception lazyloading

--- src/folder-structure/exceptions/lazyLoading-exception.ts
+++ src/folder-structure/exceptions/lazyLoading-exception.ts
@@ -3,7 +3,7 @@
 export class LazyLoadingException extends Error {
 
     constructor(message: string = 'Generic Error', routerModule: string = 'Not found') {
-        super(`LazyLoadingException: ${message}. Router module found: ${routerModule}`);
+        super(`LazyLoadingException: ${message}. Router module error: ${routerModule}`);
     }
 
 }
