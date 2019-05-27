547b57a5777da9434b1c0ab1b861ce9bacf96df2
Aaron Velasco Lopez <avelascl@everis.com>
2019-05-27 13:03:16 +0200

    correct folder structure test

--- src/folder-structure/test-cases/app-structure-test.ts
+++ src/folder-structure/test-cases/app-structure-test.ts
@@ -7,9 +7,10 @@ import { FolderStructureException } from '../exceptions/folder-structure-excepti
 export class AppStructureTest implements ITestCase {
 
     description: string;
-
+    folders: string[];
     constructor() {
         this.description = 'The app folder should only have: core, shared and modules folders';
+        this.folders = ['core', 'shared', 'modules'];
     }
 
     run(context: IContext): Promise<string> {
@@ -22,11 +23,11 @@ export class AppStructureTest implements ITestCase {
             }
             fs.readdir(directoryPath, (err, files) => {
                 if (err) throw new FolderStructureException(err.message, files ? files.join(' ') : '');
-                if (files.length === 3 && files.includes('core') &&
-                    files.includes('shared') && files.includes('modules')) {
+                const dirs = files.filter(file => fs.statSync(path.join(directoryPath, file)).isDirectory());
+                if (dirs.length === this.folders.length && dirs.every(dir => this.folders.includes(dir))) {
                     resolve();
                 } else {
-                    reject(new FolderStructureException(this.description, files.join(' ')));
+                    reject(new FolderStructureException(this.description, files.join('\n')));
                 }
             });
         });
